6a5325b237202962bc66e7205604dc695bab3749
Patcharapong10 <patcharapong20new@gmail.com>
2021-02-26 22:29:54 +0700

    commit

---
+++ Intern1.py
@@ -0,0 +1,22 @@
+import keyboard
+
+print("Internship")
+x = int(input("Enter you number : "))
+
+word = []
+for i in range(x) :
+    letterinit = ''
+    letter = input('Input your word : ')
+    letter_ = letter.split(' ')
+    for n in letter_:
+        if(n[0].isupper):
+            letterinit += n[0]
+    word.append(letterinit)
+
+word.sort(key=lambda item: (-len(item), item))
+#word.sort(reverse=True)
+for j in word:
+    print(i)
+    #print(letter)
+print(word)
+#print("Hello : " + letter)
